601cb08a6f56a475d1887b0f99160b2a8869d7f0
Elliott Stein <estein1988@gmail.com>
2020-07-22 16:39:03 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,8 +1,16 @@
-def find_item_by_name_in_collection(name, collection)
-  # Implement me first!
-  #
-  # Consult README for inputs and outputs
+require 'pry'
 
+grocery_shelf = [
+  {:item => "CANNED BEANS", :price => 3.00, :clearance => true},
+  {:item => "CANNED CORN", :price => 2.50, :clearance => false},
+  {:item => "SALSA", :price => 1.50, :clearance => false},
+  {:item => "TORTILLAS", :price => 2.00, :clearance => false},
+  {:item => "HOT SAUCE", :price => 1.75, :clearance => false}
+]
+
+
+def find_item(name)
+  
 end
 
 def consolidate_cart(cart)
@@ -11,7 +19,5 @@ def consolidate_cart(cart)
   # REMEMBER: This returns a new Array that represents the cart. Don't merely
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
 
-end
-
-
+end 
   
